948a26f67a757f0c26d044ed75802707adfa7328
Leo <leojunebedeo@gmail.com>
2023-08-26 10:06:01 +0800

    Install bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  css: [
+    'assets/scss/bootstrap.scss',
+    '~/assets/scss/main.scss'
+  ],
+  plugins: [
+    { src: '~/plugins/bootstrap.js', mode: 'client'}
+  ]
 })
