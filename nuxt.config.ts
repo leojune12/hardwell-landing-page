6b5582d35ca3de6c7f37e2225cf33ba8948aa74b
Leo June <leojunebedeo@gmail.com>
2023-08-28 16:33:39 +0800

    Update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   css: [
-    'assets/scss/bootstrap.scss',
+    '~/assets/scss/bootstrap.scss',
     '~/assets/scss/main.scss'
   ],
   plugins: [
