8b284a29dedda5653a46bba86272d72e5be14f95
Leo <leojunebedeo@gmail.com>
2023-08-28 15:11:01 +0800

    Update service items spacing

--- components/Service.vue
+++ components/Service.vue
@@ -7,43 +7,49 @@
             <div class="secondary-text text-base text-md-xl mb-5 mx-auto">
                 The sky was cloudless and of a deep dark blue spectacle before us was indeed sublime.
             </div>
-            <div class="row services g-5 pt-0 pt-md-5">
+            <div class="row services gy-5 pt-0 pt-md-5">
                 <div class="col-md-4">
-                    <div class="mb-4 d-flex justify-content-center justify-content-md-start">
-                        <img src="/images/athletics.png" alt="">
-                    </div>
-                    <div>
-                        <div class="title text-center text-md-start">
-                            Product Management
+                    <div class="item mx-auto">
+                        <div class="mb-4 d-flex justify-content-center justify-content-md-start">
+                            <img src="/images/athletics.png" alt="">
                         </div>
-                        <div class="description text-center text-md-start">
-                            Stay on top of your task lists and stay in touch with what's happening
+                        <div>
+                            <div class="title text-center text-md-start">
+                                Product Management
+                            </div>
+                            <div class="description text-center text-md-start">
+                                Stay on top of your task lists and stay in touch with what's happening
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="mb-4 d-flex justify-content-center justify-content-md-start">
-                        <img src="/images/archery-target.png" alt="">
-                    </div>
-                    <div>
-                        <div class="title text-center text-md-start">
-                            Design & Vreatives
+                    <div class="item mx-auto">
+                        <div class="mb-4 d-flex justify-content-center justify-content-md-start">
+                            <img src="/images/archery-target.png" alt="">
                         </div>
-                        <div class="description text-center text-md-start">
-                            Stay on top of your task lists and stay in touch with what's happening
+                        <div>
+                            <div class="title text-center text-md-start">
+                                Design & Vreatives
+                            </div>
+                            <div class="description text-center text-md-start">
+                                Stay on top of your task lists and stay in touch with what's happening
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="mb-4 d-flex justify-content-center justify-content-md-start">
-                        <img src="/images/money-coins.png" alt="">
-                    </div>
-                    <div>
-                        <div class="title text-center text-md-start">
-                            Markething & Commuication
+                    <div class="item mx-auto">
+                        <div class="mb-4 d-flex justify-content-center justify-content-md-start">
+                            <img src="/images/money-coins.png" alt="">
                         </div>
-                        <div class="description text-center text-md-start">
-                            Stay on top of your task lists and stay in touch with what's happening
+                        <div>
+                            <div class="title text-center text-md-start">
+                                Markething & Commuication
+                            </div>
+                            <div class="description text-center text-md-start">
+                                Stay on top of your task lists and stay in touch with what's happening
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -80,23 +86,28 @@ export default {
     }
 
     .services {
-        .title {
-            color: var(--set-01-color-03, #25373F);
-            font-family: Epilogue;
-            font-size: 22px;
-            font-style: normal;
-            font-weight: 600;
-            line-height: 34px; /* 154.545% */
-        }
+        
+        .item {
+            max-width: 350px;
 
-        .description {
-            color: var(--set-01-color-03, #25373F);
-            /* Regular/16px */
-            font-family: Inter;
-            font-size: 16px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 28px; /* 175% */
+            .title {
+                color: var(--set-01-color-03, #25373F);
+                font-family: Epilogue;
+                font-size: 22px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: 34px; /* 154.545% */
+            }
+
+            .description {
+                color: var(--set-01-color-03, #25373F);
+                /* Regular/16px */
+                font-family: Inter;
+                font-size: 16px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: 28px; /* 175% */
+            }
         }
     }
 </style>
